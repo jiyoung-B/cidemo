8515572d34738c68ca8578879338e595b8b19919
jiyoung-B <catgirl0313@naver.com>
2023-12-15 14:58:03 +0900

    script 경로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,34 +17,18 @@ else
 
   while pgrep -fla java | grep cidemo > /dev/null; do
       echo "기존 프로세스가 종료될 때까지 대기 중..."
-      sleep 1
+      sleep 3
   done
   echo "기존 프로세스가 종료되었습니다."
-#  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -t $REPOSITORY/build/libs/*-SNAPSHOT.jar | head -n 1)
-#JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*-SNAPSHOT.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
 if [ ! -f "$JAR_NAME" ]; then
   echo "실행할 JAR 파일을 찾을 수 없습니다: $JAR_NAME"
   exit 1
-fi
-
-#echo "> $JAR_NAME 에 실행권한 추가"
-#chmod +x $JAR_NAME
-# 쓰기 권한이 있는지 확인하고 없으면 부여
-if [ ! -w $REPOSITORY ]; then
-  echo "권한이 없어서 부여합니다."
-  chmod +w $REPOSITORY
-fi
-
-# 디렉토리가 없으면 생성
-if [ ! -d $REPOSITORY ]; then
-  echo "디렉토리가 없어서 생성합니다."
-  mkdir -p $REPOSITORY
 fi
 
 echo "> $JAR_NAME 실행"
@@ -53,4 +37,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME > /tmp/nohup.out 2>&1 &
 
 echo "배포 스크립트가 성공적으로 완료되었습니다."
-tail -n 30 $REPOSITORY/nohup.out
+tail -n 30 /tmp/nohup.out
